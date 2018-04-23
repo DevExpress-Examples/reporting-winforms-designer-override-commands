53dcbd6cb62726cbbd51d881ed955382bb1cefe3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomSavingEUD/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraReports.UserDesigner;
+
+namespace CustomSavingEUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        // Create an MDI (multi-document interface) controller instance.
+        XRDesignMdiController mdiController;
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create a design form and get its MDI controller.
+            XRDesignForm form = new XRDesignForm();
+            mdiController = form.DesignMdiController;
+
+            // Handle the DesignPanelLoaded event of the MDI controller,
+            // to override the SaveCommandHandler for every loaded report.
+            mdiController.DesignPanelLoaded +=
+                new DesignerLoadedEventHandler(mdiController_DesignPanelLoaded);
+
+            // Open an empty report in the form.
+            mdiController.OpenReport(new XtraReport1());
+
+            // Show the form.
+            form.ShowDialog();
+            if (mdiController.ActiveDesignPanel != null) {
+                mdiController.ActiveDesignPanel.CloseReport();
+            }
+        }
+        void mdiController_DesignPanelLoaded(object sender, DesignerLoadedEventArgs e) {
+            XRDesignPanel panel = (XRDesignPanel)sender;
+            panel.AddCommandHandler(new SaveCommandHandler(panel));
+        }
+
+        public class SaveCommandHandler : DevExpress.XtraReports.UserDesigner.ICommandHandler {
+            XRDesignPanel panel;
+
+            public SaveCommandHandler(XRDesignPanel panel) {
+                this.panel = panel;
+            }
+
+            public void HandleCommand(DevExpress.XtraReports.UserDesigner.ReportCommand command,
+            object[] args) {
+                // Save the report.
+                Save();
+            }
+
+            public bool CanHandleCommand(DevExpress.XtraReports.UserDesigner.ReportCommand command,
+            ref bool useNextHandler) {
+                useNextHandler = !(command == ReportCommand.SaveFile ||
+                    command == ReportCommand.SaveFileAs);
+                return !useNextHandler;
+            }
+
+            void Save() {
+                // Write your custom saving here.
+                // ...
+
+                // For instance:
+                panel.Report.SaveLayout("c:\\report1.repx");
+
+                // Prevent the "Report has been changed" dialog from being shown.
+                panel.ReportState = ReportState.Saved;
+            }
+        }
+    }
+}
---
+++ VB/CustomSavingEUD/Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UserDesigner
+
+Namespace CustomSavingEUD
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        ' Create an MDI (multi-document interface) controller instance.
+        Private mdiController As XRDesignMdiController
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            ' Create a design form and get its MDI controller.
+            Dim form As New XRDesignForm()
+            mdiController = form.DesignMdiController
+
+            ' Handle the DesignPanelLoaded event of the MDI controller,
+            ' to override the SaveCommandHandler for every loaded report.
+            AddHandler mdiController.DesignPanelLoaded, AddressOf mdiController_DesignPanelLoaded
+
+            ' Open an empty report in the form.
+            mdiController.OpenReport(New XtraReport1())
+
+            ' Show the form.
+            form.ShowDialog()
+            If mdiController.ActiveDesignPanel IsNot Nothing Then
+                mdiController.ActiveDesignPanel.CloseReport()
+            End If
+        End Sub
+        Private Sub mdiController_DesignPanelLoaded(ByVal sender As Object, ByVal e As DesignerLoadedEventArgs)
+            Dim panel As XRDesignPanel = DirectCast(sender, XRDesignPanel)
+            panel.AddCommandHandler(New SaveCommandHandler(panel))
+        End Sub
+
+        Public Class SaveCommandHandler
+            Implements DevExpress.XtraReports.UserDesigner.ICommandHandler
+
+            Private panel As XRDesignPanel
+
+            Public Sub New(ByVal panel As XRDesignPanel)
+                Me.panel = panel
+            End Sub
+
+            Public Sub HandleCommand(ByVal command As DevExpress.XtraReports.UserDesigner.ReportCommand, ByVal args() As Object) Implements ICommandHandler.HandleCommand
+                ' Save the report.
+                Save()
+            End Sub
+
+            Public Function CanHandleCommand(ByVal command As DevExpress.XtraReports.UserDesigner.ReportCommand, ByRef useNextHandler As Boolean) As Boolean Implements ICommandHandler.CanHandleCommand
+                useNextHandler = Not (command = ReportCommand.SaveFile OrElse command = ReportCommand.SaveFileAs OrElse command = ReportCommand.Closing)
+                Return Not useNextHandler
+            End Function
+
+            Private Sub Save()
+                ' Write your custom saving here.
+                ' ...
+
+                ' For instance:
+                panel.Report.SaveLayout("c:\report1.repx")
+
+                ' Prevent the "Report has been changed" dialog from being shown.
+                panel.ReportState = ReportState.Saved
+            End Sub
+        End Class
+    End Class
+End Namespace
