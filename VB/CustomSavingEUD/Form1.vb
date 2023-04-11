53f67c534c591512e69f61c24b4d4cc3a8b80cde
DevExpressExampleBot <fakeEmail>
2023-04-11 16:04:06 +0400

    Source auto update [skip ci]

--- VB/CustomSavingEUD/Form1.vb
+++ VB/CustomSavingEUD/Form1.vb
@@ -1,75 +1,68 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraReports.UserDesigner
+
 ' ...
-
 Namespace CustomSavingEUD
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		' Create an MDI (multi-document interface) controller instance.
-		Private mdiController As XRDesignMdiController
+    Public Partial Class Form1
+        Inherits Form
 
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			' Create a design form and get its MDI controller.
-			Dim form As New XRDesignForm()
-			mdiController = form.DesignMdiController
+        ' Create an MDI (multi-document interface) controller instance.
+        Private mdiController As XRDesignMdiController
 
-			' Handle the DesignPanelLoaded event of the MDI controller,
-			' to override the SaveCommandHandler for every loaded report.
-			AddHandler mdiController.DesignPanelLoaded, AddressOf mdiController_DesignPanelLoaded
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a design form and get its MDI controller.
+            Dim form As XRDesignForm = New XRDesignForm()
+            mdiController = form.DesignMdiController
+            ' Handle the DesignPanelLoaded event of the MDI controller,
+            ' to override the SaveCommandHandler for every loaded report.
+            AddHandler mdiController.DesignPanelLoaded, New DesignerLoadedEventHandler(AddressOf mdiController_DesignPanelLoaded)
+            ' Open an empty report in the form.
+            mdiController.OpenReport(New XtraReport1())
+            ' Show the form.
+            form.ShowDialog()
+            If mdiController.ActiveDesignPanel IsNot Nothing Then
+                mdiController.ActiveDesignPanel.CloseReport()
+            End If
+        End Sub
 
-			' Open an empty report in the form.
-			mdiController.OpenReport(New XtraReport1())
-
-			' Show the form.
-			form.ShowDialog()
-			If mdiController.ActiveDesignPanel IsNot Nothing Then
-				mdiController.ActiveDesignPanel.CloseReport()
-			End If
-		End Sub
-
-		Private Sub mdiController_DesignPanelLoaded(ByVal sender As Object, ByVal e As DesignerLoadedEventArgs)
-			Dim panel As XRDesignPanel = CType(sender, XRDesignPanel)
-			mdiController.AddCommandHandler(New SaveCommandHandler(panel))
-		End Sub
+        Private Sub mdiController_DesignPanelLoaded(ByVal sender As Object, ByVal e As DesignerLoadedEventArgs)
+            Dim panel As XRDesignPanel = CType(sender, XRDesignPanel)
+            mdiController.AddCommandHandler(New SaveCommandHandler(panel))
+        End Sub
 
         Public Class SaveCommandHandler
-            Implements DevExpress.XtraReports.UserDesigner.ICommandHandler
+            Implements ICommandHandler
+
             Private panel As XRDesignPanel
 
             Public Sub New(ByVal panel As XRDesignPanel)
                 Me.panel = panel
             End Sub
 
-            Public Sub HandleCommand(ByVal command As DevExpress.XtraReports.UserDesigner.ReportCommand, _ 
-            ByVal args() As Object) Implements DevExpress.XtraReports.UserDesigner.ICommandHandler.HandleCommand
+            Public Sub HandleCommand(ByVal command As ReportCommand, ByVal args As Object()) Implements ICommandHandler.HandleCommand
                 ' Save the report.
                 Save()
             End Sub
 
-            Public Function CanHandleCommand(ByVal command As DevExpress.XtraReports.UserDesigner.ReportCommand, _ 
-                        ByRef useNextHandler As Boolean) As Boolean _ 
-                        Implements DevExpress.XtraReports.UserDesigner.ICommandHandler.CanHandleCommand
-                            useNextHandler = Not (command = ReportCommand.SaveFile _ 
-                                    OrElse command = ReportCommand.SaveFileAs OrElse command = ReportCommand.Closing)
+            Public Function CanHandleCommand(ByVal command As ReportCommand, ByRef useNextHandler As Boolean) As Boolean Implements ICommandHandler.CanHandleCommand
+                useNextHandler = Not(command = ReportCommand.SaveFile OrElse command = ReportCommand.SaveFileAs OrElse command = ReportCommand.Closing)
                 Return Not useNextHandler
             End Function
 
             Private Sub Save()
                 ' Write your custom saving here.
                 ' ...
-
                 ' For instance:
                 panel.Report.SaveLayout("c:\report1.repx")
-
                 ' Prevent the "Report has been changed" dialog from being shown.
                 panel.ReportState = ReportState.Saved
             End Sub
         End Class
-	End Class
+    End Class
 End Namespace
--- VB/CustomSavingEUD/Properties/AssemblyInfo.vb
+++ VB/CustomSavingEUD/Properties/AssemblyInfo.vb
@@ -1,28 +1,23 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("CustomSavingEUD")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("CustomSavingEUD")>
-<Assembly: AssemblyCopyright("Copyright ©  2012")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("CustomSavingEUD")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("CustomSavingEUD")>
+<Assembly:AssemblyCopyright("Copyright ©  2012")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("a378cb00-0a85-4488-bf41-d1b36e063f00")>
-
+<Assembly:Guid("a378cb00-0a85-4488-bf41-d1b36e063f00")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -33,5 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
